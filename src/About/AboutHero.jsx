9e9facd0c5839afb15ff16aa75e4dbb519240a55
Muhammad Asif <greenparriot5@gamil.com>
2026-09-26 15:50:44 +0400

    updated

--- src/About/AboutHero.jsx
+++ src/About/AboutHero.jsx
@@ -93,7 +93,7 @@ function AboutHero() {
           </div>
 
         </div>
-      </div>
+      </div> 
     </section>
   );
 }
--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -6,7 +6,7 @@ function Blog() {
   return (
     <>
       <Helmet>
-        <title>Teen Patti Gold Blog | 3 Patti Guides, Rules & Tips</title>
+        <title>Teen Patti Gold Guide, 3 Patti Game Guide, Teen Patti Tips, Teen Patti Rules</title>
         <meta name="description" content="Explore Teen Patti Gold blog, 3 Patti rules, card rankings, gameplay basics, download guidance and responsible gaming information." />
         <link rel="canonical" href="https://teen3pattigold.net/blog" />
       </Helmet>
